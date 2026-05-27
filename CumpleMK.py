ebb89ff237dd56abf2b2f563695db6762c2005f2
DannyRuleZ <101912775+DannyRuleZ@users.noreply.github.com>
2026-05-26 23:24:09 -0400

    Update CumpleMK.py

--- CumpleMK.py
+++ CumpleMK.py
@@ -46,24 +46,12 @@ Hoy no quería darte algo común...
 </p>
 
 <p>
-Quería hacerte algo diferente, algo que pudieras abrir justo a medianoche
-y que te sacara una sonrisa aunque sea pequeñita.
+Quería hacerte algo diferente, que te sacara una sonrisa aunque sea pequeñita.
 </p>
 
 <p>
 No soy muy bueno expresando todo lo que siento,
 pero quiero que sepas que eres una personita muy especial para mí.
-</p>
-
-<p>
-Y aunque a veces no te lo diga tanto...
-me encanta decirte <b>mi vida</b> ❤️
-</p>
-
-<p>
-Espero que hoy tengas un cumpleaños hermoso,
-lleno de momentos bonitos, abrazos sinceros
-y muchas razones para sonreír.
 </p>
 
 <h3>
@@ -95,32 +83,39 @@ if st.button("Presiona aquí 🎁"):
     st.markdown("""
     <div class="card">
 
-    <h2>❤️ Para ti ❤️</h2>
+    <h2>❤️ Para Mika ❤️</h2>
 
     <p>
-    Gracias por aparecer en mi vida.
+    Espero que hoy tengas un cumpleaños hermoso,
+    lleno de momentos bonitos, muchas razones para 
+    sonreír y que sepas que hay alguien te que quiere
+    </p>
+    
+    <p>
+    Y aunque a veces no te lo diga tanto...
+    me encanta decirte <b>mi vida</b> ❤️
     </p>
 
     <p>
     Me gusta muchísimo pasar tiempo contigo,
-    hablar contigo y simplemente verte feliz.
+    hablar contigo y simplemente verte feliz
     </p>
 
     <p>
     Tal vez esta página no sea perfecta,
     pero la hice pensando en ti...
-    y eso la hace especial para mí.
+    y eso la hace especial para mí
     </p>
 
     <p>
     Ojalá este nuevo año de vida te traiga
     muchas alegrías, metas cumplidas
     y personas que te quieran tan bonito
-    como mereces.
+    como mereces
     </p>
-
+    
     <h3>
-    Te quiero muchísimo ❤️
+    Gracias por aparecer en mi vida
     </h3>
 
     </div>
@@ -131,6 +126,6 @@ st.write("---")
 
 st.markdown("""
 <h4 style='text-align:center;'>
-Hecho con cariño para alguien muy especial 🌹
+TU te mereces todo lo mejor 🌹
 </h4>
 """, unsafe_allow_html=True)
