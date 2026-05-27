a31ccfbb05b95995a725848110cc48f82770c391
DannyRuleZ <101912775+DannyRuleZ@users.noreply.github.com>
2026-05-26 23:29:30 -0400

    Update CumpleMK.py

--- CumpleMK.py
+++ CumpleMK.py
@@ -72,7 +72,7 @@ if st.button("Presiona aquí 🎁"):
 
     # AUDIO AUTOMATICO
     with open("Chino & Nacho - Niña Bonita (Original Version).mp3", "rb") as audio_file:
-    audio_bytes = audio_file.read()
+        audio_bytes = audio_file.read()
 
     st.audio(audio_bytes, format="audio/mp3", autoplay=True)
 
